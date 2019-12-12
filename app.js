08e1cb28a9690dea59ce79df8c0c9fdb7f572d15
Robert Greenstreet <robertgreenstreet@gmail.com>
2019-12-12 20:58:41 +0000

    removed db password from .env, added .env to .gitignore, added 'production' and 'development' variables to package.json scripts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+
+require('dotenv').config();
 const 	express 				= require("express"),
 		app 					= express(),
-	  	dotEnv					= require('dotenv').config(),
 		LocalStrategy			= require('passport-local'),
 	  	bodyParser 				= require('body-parser'),
 		mongoose 				= require('mongoose'),
